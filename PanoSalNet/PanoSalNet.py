51b7e23a019b053ba3b64ffe37b65f82b1bab082
Anh Phan Nguyen <phananh1010@yahoo.com>
2018-09-01 17:32:13 -0400

    Update PanoSalNet.py
    Model weights, model parameters

--- PanoSalNet/PanoSalNet.py
+++ PanoSalNet/PanoSalNet.py
@@ -1,11 +1,11 @@
 
 #TODO: load PanoSalNet and test on one image
-import caffe
-import pickle
+#input: prototxt file, caffemodel file, test img file
+#output: salient
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
+import caffe
+import timeit
 
 def post_filter(_img):
     result = np.copy(_img)
@@ -15,23 +15,26 @@ def post_filter(_img):
     result[-3:, -3:] = _img.min()
     return result
 
-if 'solver' not in locals():
-    solver = caffe.SGDSolver('./PanoSalNet_solver.prototxt')
-    #solver.net.copy_from('./states/salnetv6_step_iter_800.caffemodel')
-    solver.test_nets[0].copy_from('./states/salnetv6_step_iter_800.caffemodel')
-    
+net = caffe.Net('./panosalnet/panosalnet_test.prototxt', caffe.TEST)
+net.copy_from('./panosalnet/panosalnet_iter_800.caffemodel')
+
+C, H, W = net.blobs['data1'].data[0].shape
+
+img_filepath = './panosalnet/test.png'
+img = cv2.imread(img_filepath)
 mu = [ 104.00698793, 116.66876762, 122.67891434]
-input_scale = 0.0078431372549    
-img0 = cv2.imread(img_file_path)
-img0 = img0.astype(np.float32)
-img0 -= np.array(mu)
-img0 *= input_scale
+input_scale = 0.0078431372549
 
-img = np.transpose(cv2.resize(np.transpose(img0, (1, 2, 0)), (384, 216)), (2, 0, 1))
-img = img.reshape(1, *img.shape)
+dat = cv2.resize(img, (W, H))
+dat = dat.astype(np.float32)
+dat -= np.array(mu)#mean values
+dat *= input_scale
+dat = dat.transpose((2,0,1))
 
-solver.test_nets[0].blobs['data1'].data[0] = img
+net.blobs['data1'].data[0] = dat
+net.forward(start='conv1')
 
-solver.test_nets[0].forward(start='conv1')
-fx = solver.test_nets[0].blobs['deconv1'].data[0][0]
-fx = post_filter(fx)
+salient = net.blobs['deconv1'].data[0][0]
+salient = (salient * 1.0 - salient.min())
+salient = (salient / salient.max()) * 255
+salient = post_filter(salient)
