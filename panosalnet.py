3ec2d35e18d07101d67810aaf28c335af78faf16
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2018-09-01 19:00:13 -0400

    add new files

---
+++ panosalnet.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import caffe
+import timeit
+
+def post_filter(_img):
+    result = np.copy(_img)
+    result[:3,:3] = _img.min()
+    result[:3, -3:] = _img.min()
+    result[-3:, :3] = _img.min()
+    result[-3:, -3:] = _img.min()
+    return result
+
+net = caffe.Net('panosalnet_test.prototxt', caffe.TEST)
+net.copy_from('panosalnet_iter_800.caffemodel')
+
+C, H, W = net.blobs['data1'].data[0].shape
+
+img_filepath = 'test.png'
+img = cv2.imread(img_filepath)
+mu = [ 104.00698793, 116.66876762, 122.67891434]
+input_scale = 0.0078431372549
+
+dat = cv2.resize(img, (W, H))
+dat = dat.astype(np.float32)
+dat -= np.array(mu)#mean values
+dat *= input_scale
+dat = dat.transpose((2,0,1))
+
+net.blobs['data1'].data[0] = dat
+net.forward(start='conv1')
+
+salient = net.blobs['deconv1'].data[0][0]
+salient = (salient * 1.0 - salient.min())
+salient = (salient / salient.max()) * 255
+salient = post_filter(salient)
